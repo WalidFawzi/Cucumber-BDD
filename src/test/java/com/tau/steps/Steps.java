edd2684418295cf437e3d39a2ec8d6c6608ba46e
WalidFawzi <w.elbastawisi@diyarme.com>
2022-06-23 12:27:38 +0300

    Scenario outline + Examples section

--- src/test/java/com/tau/steps/Steps.java
+++ src/test/java/com/tau/steps/Steps.java
@@ -1,5 +1,8 @@
 package com.tau.steps;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,34 +14,43 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.util.List;
+
 public class Steps {
 	private WebDriver driver;
+	@Before
+	public void setup(){
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+	}
 	
 	@Given("I am in the login page of the Para Bank Application")
 	public void i_am_in_the_login_page_of_the_Para_Bank_Application() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
 		driver.get("https://parabank.parasoft.com/parabank/index.htm");
 		
 	    
 	}
 
 	@When("I enter valid credentials")
-	public void i_enter_valid_credentials() {
-		driver.findElement(By.name("username")).sendKeys("Waleed");
-		driver.findElement(By.name("password")).sendKeys("Waleed@2028");
+	public void i_enter_valid_credentials(DataTable table) {
+		List <String> loginForm = table.asList();
+		driver.findElement(By.name("username")).sendKeys(loginForm.get(0));
+		driver.findElement(By.name("password")).sendKeys(loginForm.get(1));
 		driver.findElement(By.name("username")).submit();
 
 	}
 
 	@Then("I should be taken to the Overview page")
 	public void i_should_be_taken_to_the_Overview_page() {
-		WebDriverWait wait = new WebDriverWait(driver, 20);
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\\rightPanel\\”]/div/div/h1")));
+		driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")).isDisplayed();
+		driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")).click();
 
-		driver.findElement(By.xpath("//*[@id=\\rightPanel\\”]/div/div/h1")).isDisplayed();
-		driver.findElement(By.linkText("Log out")).click();
-		driver.quit();	
+	}
+
+	@After
+	public void tearDown (){
+		driver.quit();
 
 	}
 
--- target/test-classes/Login.feature
+++ target/test-classes/Login.feature
@@ -5,9 +5,10 @@ Feature: Login Functionality
 	I want to login successfully
 
 # Scenario Example
-@Login	
+@Login
 Scenario: Login Successful
 
 Given I am in the login page of the Para Bank Application
 When I enter valid credentials
+	|"Waleed"|Waleed@2028|
 Then I should be taken to the Overview page
